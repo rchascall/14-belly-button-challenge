aed81af8bffb3eb20afb1182331b8480d6590a1c
rchascall <robert.hascall@outlook.com>
2023-02-11 14:59:36 -0800

    bar chart progress

--- static/js/app.js
+++ static/js/app.js
@@ -5,11 +5,42 @@ const bellybutton_url = "https://2u-data-curriculum-team.s3.amazonaws.com/datavi
 d3.json(bellybutton_url).then(function(data) {
 console.log(data);
 
-  // Create dropdown menu
-  //let dropdown = d3.select("#selDataset")
-  //  .selectAll("option")
-  //  .data(data.names)
+  // Create dropdown menu and create elements in dropdown for all "options" from names array
+  let dropdown = d3.select("#selDataset")
+    .selectAll("option")
+    .data(data.names)
+    .enter()
+    .append("option")
+    .text(function(x) {
+      return x;
+    })
 
+  // Display first name on page load
+  getData(data.names[0]);
 
+  // Create Function called by DOM changes
+  function getData(selectedName) {
 
+    // Filter for selected name
+    let nameData = data.samples.filter(sample => sample.id === selectedName)[0];
+
+    // Update bar chart
+    let barChart = [{
+      x: nameData.sample_values.slice(0,10).reverse(),
+      y: nameData.otu_ids.slice(0,10).map(otu_id => "OTU " + otu_id).reverse(),
+      text: nameData.otu_labels.slice(0,10).reverse(),
+      orientation: "h",
+      type: "bar"
+    }];
+
+    Plotly.newPlot("bar", barChart);
+    
+  }  
+
+  // Call getData() when a change takes place to the DOM
+  d3.select("#selDataset").on("change", function() {
+    let selectedName = d3.select(this).property("value");
+    getData(selectedName);
+
+  })
 });
